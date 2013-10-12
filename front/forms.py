2ec5d20026461b2f15ec7bf446085ad4c98f9775
Bl3f <christophe.blefari@gmail.com>
2013-10-12 12:00:59 +0200

    Add static js structure

--- front/forms.py
+++ front/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class ContactForm(forms.Form):
     subject = forms.CharField()
     email = forms.CharField(required=False)
-    message = forms.CharField()
+    message = forms.CharField(widget=forms.Textarea)
